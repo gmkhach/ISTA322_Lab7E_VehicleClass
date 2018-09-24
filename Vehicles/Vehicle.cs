0117323735d3eb735fa2d50ec884f1160fc8a584
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-23 21:26:31 -0400

    Add project files.

---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    class Vehicle
+    {
+        public void StartEngine(string noiseToMakeWhenStarting)
+        {
+            Console.WriteLine($"Starting engine: {noiseToMakeWhenStarting}");
+        }
+        public void StopEngine(string noiseToMakeWhenStopping)
+        {
+            Console.WriteLine($"Stopping engine: {noiseToMakeWhenStopping}");
+        }
+    }
+}
